7611ad841baa4ecd518f81bee11d3f92bf8c086a
Osiris Then <osiriscuevas@hotmail.com>
2018-09-19 02:48:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-//  var newObj = Object.assign({}, object)
-  delete object.key
-  return object;
+  var newObj = Object.assign({}, object)
+  delete newObj.key
+  return newObj;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
