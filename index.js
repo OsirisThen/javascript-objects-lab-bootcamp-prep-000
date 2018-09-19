f30221c16cca24bec7f393346e83a601f8003e9f
Osiris Then <osiriscuevas@hotmail.com>
2018-09-19 02:22:36 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+var recipes = {
+  prop: '1'
+}
+
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key]=value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({}, object)
+  delete newObj.key;
+  return object;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+ //object = { klk: 3 }
+
+  return object;
+}
